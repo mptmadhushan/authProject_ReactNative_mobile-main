4c033ca0cfac8adbdecd6ea1b01ef6a33270c18e
tharindu_107365 <tharindu.madhushan@axiatadigitallabs.com>
2023-12-10 21:53:50 +0530

    MenuList fixes

--- app/AuthScreen.js
+++ app/AuthScreen.js
@@ -2,154 +2,117 @@ import React from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
-  Button,
   TouchableHighlight,
   ImageBackground,
   Image,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import ReactNativeBiometrics,{ BiometryTypes } from 'react-native-biometrics';
+import ReactNativeBiometrics, {BiometryTypes} from 'react-native-biometrics';
 import database from '@react-native-firebase/database';
 
 import {useEffect, useState} from 'react';
 
-
-
-const AuthScreen= ({route, navigation}) => {
+const AuthScreen = ({route, navigation}) => {
   const isDarkMode = useColorScheme() === 'dark';
-  const [userId, setUserId] = useState('')
-
- // const [isBiometricSupported, setIsBiometricSupported] = React.useState(false);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const compatible = await LocalAuthentication.hasHardwareAsync();
-  //     setIsBiometricSupported(compatible);
-  //   })();
-  // });
+  const [userId, setUserId] = useState('');
 
   React.useEffect(() => {
     if (route.params?.userId) {
-      setUserId(route.params?.userId)
-     
+      setUserId(route.params?.userId);
     }
   }, [route.params?.userId]);
 
-;
-  const rnBiometrics = new ReactNativeBiometrics({ allowDeviceCredentials: true })
+  const rnBiometrics = new ReactNativeBiometrics({
+    allowDeviceCredentials: true,
+  });
 
-  const rnFaceToutchID = () =>{
-    rnBiometrics.simplePrompt({
-        promptMessage: "Sign in with Touch ID",
-        cancelButtonText: "Close",
-        })
-        .then(async (resultObject) => {
-        // let allData = await resultObject
-        // console.trace("🚀 ~ allData", allData)
-        const { success } = resultObject;
+  const rnFaceToutchID = () => {
+    rnBiometrics
+      .simplePrompt({
+        promptMessage: 'Sign in with Touch ID',
+        cancelButtonText: 'Close',
+      })
+      .then(async resultObject => {
+        const {success} = resultObject;
         if (success) {
-        console.trace("successful biometrics provided");
-        updateUserStateParam()
+          console.trace('successful biometrics provided');
+          updateUserStateParam();
         } else {
-        console.trace("user cancelled biometric prompt");
-        // this.addListnerAgain()
+          console.trace('user cancelled biometric prompt');
         }
-        })
-        .catch((err) => {
-        console.trace("biometrics failed", err);
-        });
-  }
+      })
+      .catch(err => {
+        console.trace('biometrics failed', err);
+      });
+  };
 
-
-  const updateUserStateParam = () =>{
+  const updateUserStateParam = () => {
     database()
-    .ref('/userrand/'+userId)
-    .update({
-      isUserLog: true,
-    })
-    .then(() => 
-    console.log('Data updated.'),
-    navigation.navigate('home')
-    )
-    .catch((error) => {
-        console.log("error: " + error);
-    })
-  }
+      .ref('/user_auth/' + userId)
+      .update({
+        isUserLog: true,
+      })
+      .then(() => console.log('Data updated.'), navigation.navigate('home'))
+      .catch(error => {
+        console.log('error: ' + error);
+      });
+  };
 
   const isBiometricSupport = async () => {
+    rnBiometrics.isSensorAvailable().then(resultObject => {
+      const {available, biometryType} = resultObject;
+      console.log(biometryType);
+      rnFaceToutchID();
 
-  
-    rnBiometrics.isSensorAvailable()
-    .then((resultObject) => {
-      const { available, biometryType } = resultObject
-      console.log(biometryType)
-      rnFaceToutchID()
-  
       if (available && biometryType === BiometryTypes.TouchID) {
-        console.log('TouchID is supported')
-        rnFaceToutchID()
+        console.log('TouchID is supported');
+        rnFaceToutchID();
       } else if (available && biometryType === BiometryTypes.FaceID) {
-        console.log('FaceID is supported')
-        rnFaceToutchID()
+        console.log('FaceID is supported');
+        rnFaceToutchID();
       } else if (available && biometryType === BiometryTypes.Biometrics) {
-        console.log('Biometrics is supported')
-        rnFaceToutchID()
+        console.log('Biometrics is supported');
+        rnFaceToutchID();
       } else {
-        console.log('Biometrics not supported')
+        console.log('Biometrics not supported');
       }
-    })
-
-   
+    });
   };
 
   return (
-    <SafeAreaView style={{justifyContent:'center',alignContent:'center'}}>
-    <ImageBackground source={require('./images/bgbiometric.jpeg')} resizeMode="cover" style={styles.image}>
-          <Text style={{fontSize: 25, justifyContent:'center', textAlign:'center', marginTop: 100}}>
-             Please click finger icon for biometric authentication
-          </Text>
-          <View style={{flex:1, marginTop: 300, alignContent:'center', alignItems:'center'}}>
-          <TouchableHighlight onPress={isBiometricSupport}>
-          <Image source={require('./images/finger.png')} style={{width: 100, height: 100, resizeMode: 'stretch',}}/>
-          </TouchableHighlight>
-        
-          </View>
-         
-    </ImageBackground>
-
-   
-        {/* <Header /> */}
-        {/* <TouchableHighlight
+    <SafeAreaView style={{justifyContent: 'center', alignContent: 'center'}}>
+      <ImageBackground
+        source={require('./images/bgbiometric.jpeg')}
+        resizeMode="cover"
+        style={styles.image}>
+        <Text
           style={{
-            height: 60,
+            fontSize: 25,
+            justifyContent: 'center',
+            textAlign: 'center',
+            marginTop: 100,
           }}>
-          <Button
-            title="Login with Biometrics"
-            color="#fe7005"
-          
-          />
-        </TouchableHighlight>
+          Please click finger icon for biometric authentication
+        </Text>
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            flex: 1,
+            marginTop: 300,
+            alignContent: 'center',
+            alignItems: 'center',
           }}>
-        
-
-        </View> */}
-
+          <TouchableHighlight onPress={isBiometricSupport}>
+            <Image
+              source={require('./images/finger.png')}
+              style={{width: 100, height: 100, resizeMode: 'stretch'}}
+            />
+          </TouchableHighlight>
+        </View>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
@@ -172,8 +135,8 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
   image: {
-    height:'100%',
-    width:'100%'
+    height: '100%',
+    width: '100%',
   },
 });
 
